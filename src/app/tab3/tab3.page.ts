0ea39688646407731c874b774571b7ff4ca15c0d
agazirce <48948789+agazirce@users.noreply.github.com>
2021-12-16 21:23:28 +0100

    tab3 call randomizer (pb with CORS)

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { HttpHeaders } from '@angular/common/http';
+import { HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-tab3',
+  styleUrls: ['tab3.page.scss'],
   templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  random: any = { }
 
-  constructor() {}
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Access-Control-Allow-Origin':'*',
+      'Authorization':'authkey',
+      'userid':'1'
+    })
+  };
 
+  constructor(private http:HttpClient) {
+    this.http = http;
+  }
+
+/*  userAPI(data): Observable<any> {
+    return this.http.get(`http://localhost:8080/api/random/${number}/${players}`, httpOptions)
+      .pipe(
+        tap((result) => console.log('result-->',result)),
+        catchError(this.handleError('error', []))
+      );
+  }*/
+  logForm() {
+    let number = this.random.nombre ? this.random.nombre : 1
+    let players = this.random.participants
+    console.log(this.http.get(`http://localhost:8080/api/random/${number}/${players}`, this.httpOptions))
+    console.log(this.random)
+  }
 }
