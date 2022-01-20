893354321b77139fd8e40545eec8b821998b2aae
agazirce <48948789+agazirce@users.noreply.github.com>
2022-01-20 13:35:56 +0100

    update display result

--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -1,9 +1,7 @@
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonButton} from '@ionic/angular';
 import { Tab3Page } from './tab3.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
@@ -15,7 +13,6 @@ import { Tab3PageRoutingModule } from './tab3-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    // RouterModule.forChild([{ path: '', component: Tab3Page }]),
     Tab3PageRoutingModule,
   ],
   declarations: [Tab3Page]
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
-import { HttpHeaders } from '@angular/common/http';
 import { HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {catchError, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-tab3',
@@ -15,26 +13,14 @@ export class Tab3Page {
   players: string
   result: any
 
-/*  httpOptions = {
-    headers: new HttpHeaders({
-      // 'Access-Control-Allow-Origin':'http://localhost:8080',
-      // 'Authorization':'authkey',
-      // 'userid':'1'
-      'Content-Type':'application/json; charset=utf-8'
-    })
-  };*/
-
-  // constructor() {}
   constructor(private http:HttpClient) {
     this.http = http;
   }
 
   userAPI(): Observable<any> {
-    return this.http.get<any>(`http://localhost:8080/api/random/${this.number}/${this.players}`/*, this.httpOptions*/)/*
-      .pipe(
-        tap((result) => console.log('result-->',result))
-      )*/;
+    return this.http.get<any>(`http://localhost:8080/api/random/${this.number}/${this.players}`);
   }
+
   logForm() {
     this.number = this.random.nombre ? this.random.nombre : 1
     this.players = this.random.participants
